113d52ab5c448fa50433544fbed56fad0d152d7b
YOMO032904 <jerolldarodomanais@gmail.com>
2025-12-01 20:19:48 +0800

    change

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,19 +1,37 @@
 const express = require('express');
+
 const router = express.Router();
+
 const {
+
   getRooms,
+
   getAvailableRooms,  // ← Add this
+
   getRoomById,
+
   createRoom,
+
   updateRoom,
+
   deleteRoom
+
 } = require('../controllers/roomController');
 
+
+
 router.get('/', getRooms);
+
 router.get('/available', getAvailableRooms);  // ← MUST be before '/:id'
+
 router.get('/:id', getRoomById);
+
 router.post('/', createRoom);
+
 router.put('/:id', updateRoom);
+
 router.delete('/:id', deleteRoom);
+
+
 
 module.exports = router;
