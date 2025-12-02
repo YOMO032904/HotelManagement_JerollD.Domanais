c5c116c579bea22d51b429c6f9f776c2063ed7a0
YOMO032904 <jerolldarodomanais@gmail.com>
2025-12-02 08:27:10 +0800

    change

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ mongoose.connect(process.env.MONGODB_URI)
   .catch(err => console.error('MongoDB connection error:', err));
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
